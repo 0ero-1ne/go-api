d8b4c2e5136971f85e97f65bd83b702b5ea671e0
0ero1ne <volikovd17@gmail.com>
2024-12-24 22:59:52 +0300

    feat(go.mod): GORM and Godotenv packages

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"simpapi/db"
+	"simpapi/envresolver"
+	"simpapi/models"
+)
 
+func main() {
+	dns := envresolver.GetKey("dns")
+
+	db := db.OpenDB(dns)
+	connection, err := db.DB()
+
+	if err != nil {
+		panic("Get DB error")
+	}
+
+	defer connection.Close()
+
+	db.AutoMigrate(&models.Country{}, &models.City{}, &models.User{})
+
+	var resultCountries []models.Country
+
+	db.Find(&resultCountries)
+
+	for k, v := range resultCountries {
+		fmt.Println("Row = ", k)
+		fmt.Println("ID =", v.ID)
+		fmt.Println("Title =", v.Title)
+		fmt.Println("CreatedAt =", v.CreatedAt)
+		fmt.Println("UpdatedAt =", v.UpdatedAt)
+		fmt.Println("-----")
+	}
 }
